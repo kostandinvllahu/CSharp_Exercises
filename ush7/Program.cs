f06529bdb56d78b596a680ebb598dceab90f6003
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-03 11:15:16 +0100

    First Week Finished

---
+++ ush7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ush7
+{
+    class Program
+    {
+
+        public static void unique(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Boolean unique = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        unique = true;
+                        break;
+                    }
+                }
+                if (!unique)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+           int [] arr = { 1, 5, 5, 5, 10, 4, 4, 3, 2 };
+            Console.WriteLine("Vlerat: ");
+            Array.Sort(arr);
+            unique(arr);
+        }
+    }
+}
---
+++ ush8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ush8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int result = 0;
+
+            Console.WriteLine("Vendos vleren e nje nr natyror");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num < 0)
+            {
+                Console.WriteLine("Vetem nr natyror!");
+            }
+            else
+            {
+                for(int i=0; i<=num; i++)
+                {
+                    result += i;
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
